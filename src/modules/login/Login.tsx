6603da62d76fee20d1fef926dc5f7c250ada8546
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2020-10-14 21:45:43 -0300

    caraio que nao vai

---
+++ src/modules/login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <h1>Ola</h1>
+    </Container>
+  );
+};
+
+export { Login };
