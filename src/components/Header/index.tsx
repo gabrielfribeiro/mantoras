dd37b8ac76a8f018b50fb0be3264ce2b93d8c633
gabrielfribeiro <gabrielfribeiro44@gmail.com>
2020-11-13 19:55:41 -0300

    Created header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Container, IconContainer } from './styles';
+import {
+  Container,
+  IconContainer,
+  SearchContainer,
+  Search,
+  Logo,
+} from './styles';
 import { FaRegUserCircle } from 'react-icons/fa';
+import { Form } from '@unform/web';
+import logo from '../../assets/images/icons/programming.png';
+
 const Header: React.FC = () => {
+  const handleSubmit = () => {
+    console.log('Busca');
+  };
+
   return (
     <Container>
+      <SearchContainer>
+        <Logo>
+          <img src={logo} />
+        </Logo>
+        <Form onSubmit={handleSubmit}>
+          <Search placeholder="Pesquisa..." />
+        </Form>
+      </SearchContainer>
       <IconContainer>
-        <FaRegUserCircle size="50px" />
+        <FaRegUserCircle size="50px" color="white" />
       </IconContainer>
     </Container>
   );
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 import { Container, Nav } from './styles';
 import { ImExit } from 'react-icons/im';
+import { HiOutlineDesktopComputer } from 'react-icons/hi';
+import { RiBookOpenLine, RiSaveLine } from 'react-icons/ri';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -13,18 +15,21 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
     <Container isOpen={isOpen}>
       <Nav>
         <NavLink to="/labs" activeClassName="active">
+          <HiOutlineDesktopComputer size="25px" />
           <span>Laboratórios</span>
         </NavLink>
         <NavLink to="/reserve-lab" activeClassName="active">
+          <RiBookOpenLine size="25px" />
           <span>Reservar Laboratórios</span>
         </NavLink>
         <NavLink to="/reserves" activeClassName="active">
+          <RiSaveLine size="25px" />
           <span>Minhas Reservas</span>
         </NavLink>
       </Nav>
       <Nav style={{ position: 'absolute', bottom: 50, width: '100%' }}>
         <NavLink to={'/'} activeClassName="active">
-          <ImExit />
+          <ImExit size="25px" />
           <span>Sair</span>
         </NavLink>
       </Nav>
